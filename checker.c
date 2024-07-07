7e58b11e91c058b4d5278ed2558d499eedfdd2ad
ManoreAshish <manoreashish@gmail.com>
2024-07-08 00:22:10 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -3,7 +3,7 @@
 
 int batteryIsOk(float temperature, float soc, float chargeRate) 
 {
-  if(temperature > 0 && temperature < 45) && (soc > 20 && soc < 80) && (chargeRate< 0.8)
+  if((temperature > 0 && temperature < 45) && (soc > 20 && soc < 80) && (chargeRate< 0.8))
     return 1;
   else
    return 0;
