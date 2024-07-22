045dab9220cbbd862d5bafb369c4b15e934b7b3d
ManoreAshish <manoreashish@gmail.com>
2024-07-22 20:04:46 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -17,25 +17,29 @@ int batteryIsOk(float temperature, float soc, float chargeRate)
 {
   if( 0<temperature<45)
     return isSOCWithinRange(soc);
-  else return 0;
+  else
+    return 0;
 }
 
 int isSOCWithinRange (float soc)
 {
   if(20<soc<80)
     return isChargeRateValid(chargeRate);
-  else return 0;
+  else 
+    return 0;
 }
 
 int isChargeRateValid (float chargeRate)
 {
   if(chargeRate<0.8)
     return 1;
-  else return 0;
+  else 
+    return 0;
 }
 
 
-int main() {
+int main() 
+{
   assert(batteryIsOk(25, 70, 0.7));
   assert(!batteryIsOk(50, 85, 0));
 }
