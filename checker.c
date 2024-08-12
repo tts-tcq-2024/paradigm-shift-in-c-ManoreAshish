e080525d9986208d6fa9ed6d4db0c35fab02a060
ManoreAshish <manoreashish@gmail.com>
2024-08-12 11:17:36 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -5,10 +5,11 @@ float temperature=0;
 float soc=0;
 float chargeRate=0;
 
-int Tempupperlimit=0;
-int Templowerlimit=45;
-int SOCupperlimit=80;
-int SOClowerlimit=20;
+float Tempupperlimit=45;
+float Templowerlimit=0;
+float SOCupperlimit=80;
+float SOClowerlimit=20;
+float ChargeRateUpperlimit=0.8
 
 int isTempWithinRange( float temperature);
 int isSOCWithinRange (float soc);
@@ -24,7 +25,7 @@ int batteryIsOk(float temperature, float soc, float chargeRate)
 
 int isTempWithinRange( float temperature)
 {
-  if((temperature>0 ) && (temperature<45))
+  if((temperature>Templowerlimit) && (temperature<Tempupperlimit))
   {
     TempWarningIndicator();
     return isSOCWithinRange(soc);
@@ -47,14 +48,17 @@ int isSOCWithinRange (float soc)
 int isChargeRateValid (float chargeRate)
 {
   if(chargeRate<0.8)
+  {
+    ChargeRateWarningIndicator();
     return 1;
+  }
   else 
     return 0;
 }
 
 void TempWarningIndicator()
 {
-  int TempTolerance=0;
+  float TempTolerance=0;
   TempTolerance=(Tempupperlimit *5)/100;
   if((Tempupperlimit-TempTolerance) < temperature < (Tempupperlimit))
     printf("High Temp Detected");
@@ -64,12 +68,20 @@ void TempWarningIndicator()
 
 void SOCWarningIndicator()
 {
-  int SOCTolerance=0;
+  float SOCTolerance=0;
   SOCTolerance=(SOCupperlimit *5)/100;
   if((SOCupperlimit-SOCTolerance) < soc < (SOCupperlimit))
     printf("Approaching charge-peak");
   if((SOClowerlimit) < soc < (SOClowerlimit+SOCTolerance))
     printf("Approaching discharge"); 
+}
+
+void ChargeRateWarningIndicator()
+{
+  float ChargeRateTolerance=0;
+  ChargeRateTolerance=(ChargeRateUpperlimit *5)/100;
+  if((ChargeRateUpperlimit-SOCTolerance) < chargeRate < (ChargeRateUpperlimit))
+    printf("Approaching threshold of Valid charge rate");
 }
 
 
